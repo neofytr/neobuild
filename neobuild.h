38c6753db0f191308bd349fd960611e9c1e5bc77
neofytr <imagination12344321.com@gmail.com>
2025-03-18 21:02:05 +0530

    neobuild.h

--- neobuild.h
+++ neobuild.h
@@ -1,2 +1,23 @@
-#include <stdlib.h>
+#ifndef NEOBUILD_H
+#define NEOBUILD_H
+
+#include "dynarr/inc/dynarr.h"
 #include <stdint.h>
+#include <stdlib.h>
+
+typedef enum
+{
+    DASH,
+    BASH,
+    SH,
+} shell_t;
+
+typedef struct
+{
+    dyn_arr_t *args;
+    shell_t shell;
+} cmd_t;
+
+cmd_t *create_cmd(shell_t shell);
+
+#endif
