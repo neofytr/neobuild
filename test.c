831ba120201830675b0cbd2c6b1de28521efd695
neofytr <imagination12344321.com@gmail.com>
2025-03-19 13:15:29 +0530

    update

--- test.c
+++ test.c
@@ -1,8 +1,12 @@
 #include "neobuild.h"
+#include <stdio.h>
 
 int main()
 {
     cmd_t *cmd = cmd_create(BASH);
     cmd_append(cmd, "clang", "-Wall", "test.c", "-o", "test");
+    char *str = cmd_render(cmd);
+    fprintf(stdout, "%s\n", str);
+    free(str);
     return EXIT_SUCCESS;
 }
