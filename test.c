2858ce0bf92d726d6df7ad5419907f685744d247
neofytr <imagination12344321.com@gmail.com>
2025-03-19 22:12:07 +0530

    removing waste

--- test.c
+++ test.c
@@ -6,6 +6,6 @@ int main()
     cmd_t *cmd = cmd_create(BASH);
     cmd_append(cmd, "clang", "-Wall", LABEL_WITH_SPACES("main hop.c"), "-o", "main", "&& ./main");
     int status, code;
-    cmd_run_sync(cmd, &status, &code, false);
+    cmd_run_sync(cmd, NULL, NULL, false);
     return EXIT_SUCCESS;
 }
