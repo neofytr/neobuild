6a85758ecbf39c885ed4d5072b8604d2c1a6c767
neofytr <imagination12344321.com@gmail.com>
2025-03-19 10:16:39 +0530

    some no build functions

--- neobuild.c
+++ neobuild.c
@@ -1,6 +1,6 @@
 #include "neobuild.h"
 
-cmd_t *create_cmd(shell_t shell)
+cmd_t *cmd_create(shell_t shell)
 {
     cmd_t *cmd = (cmd_t *)malloc(sizeof(cmd_t));
     if (!cmd)
@@ -11,6 +11,26 @@ cmd_t *create_cmd(shell_t shell)
 #define MIN_ARG_NUM 8
     cmd->args = dyn_arr_create(MIN_ARG_NUM, sizeof(char *), NULL);
 #undef MIN_ARG_NUM
+    if (!cmd->args)
+    {
+        free(cmd);
+        return NULL;
+    }
+    cmd->shell = shell;
 
-
+    return cmd;
 }
+
+bool cmd_delete(cmd_t *cmd)
+{
+    if (!cmd || !cmd->args)
+    {
+        return false;
+    }
+
+    dyn_arr_free(cmd->args);
+    free((void *)cmd);
+
+    return true;
+}
+
