ce96401e9412af96525692bda4bba2ef2aa51d4d
neofytr <imagination12344321.com@gmail.com>
2025-03-20 14:17:33 +0530

    added rebuilding functionality

--- neo.c
+++ neo.c
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
 {
     neorebuild("neo.c", argv);
     neocmd_t *cmd = neocmd_create(BASH);
-    neocmd_append(cmd, "clang", "-Wall", "tempor.c", "-o", "main", "&& ./main");
+    neocmd_append(cmd, "clang", "-Wall", "temporary.c", "-o", "main", "&& ./main");
     int status, code;
     neocmd_run_sync(cmd, NULL, NULL, false);
     return EXIT_SUCCESS;
