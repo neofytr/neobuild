6c1a4e1d687ea3067c9fd7c529938320dc599838
neofytr <imagination12344321.com@gmail.com>
2025-03-21 11:06:01 +0530

    compile function working properly

--- neo.c
+++ neo.c
@@ -4,6 +4,6 @@
 int main(int argc, char **argv)
 {
     neorebuild("neo.c", argv);
-    neo_compile_to_object_file(GCC, "main.c", NULL, NULL, false);
+    neo_compile_to_object_file(GCC, "main.c", NULL, NULL, true);
     return EXIT_SUCCESS;
 }
